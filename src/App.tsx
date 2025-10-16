828da2bf0e0fd2846d4aec86e2967d215342956a
Mario <marionaiaferreira@gmail.com>
2025-10-16 18:38:52 +0100

    Initial commit with Vite + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen grid place-items-center bg-slate-900 text-white text-4xl">
+      Tailwind is working ✨
+    </div>
+  );
+}
